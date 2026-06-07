c46305f059a550caaf4c06f500a6d7cd4e952cd8
Sameer <sameeridea7@gmail.com>
2026-06-07 15:38:05 +0530

    Implement keyboard actions in Test27_KeyBoardActions class

--- src/test/java/DaySelenium1/Test27_KeyBoardActions.java
+++ src/test/java/DaySelenium1/Test27_KeyBoardActions.java
@@ -1,4 +1,45 @@
 package DaySelenium1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
 public class Test27_KeyBoardActions {
+    static void main(String[] args){
+        String url = "https://testautomationpractice.blogspot.com/#";
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.manage().window().maximize();
+        driver.get(url);
+
+        driver.findElement(By.name("input1")).sendKeys("Sameer");
+        Actions actions = new Actions(driver);
+        //control.A
+        actions.keyDown(Keys.CONTROL)
+                .sendKeys("A")
+                .keyUp(Keys.CONTROL)
+                .perform();
+        //control.C
+        actions.keyDown(Keys.CONTROL)
+                .sendKeys("C")
+                .keyUp(Keys.CONTROL)
+                .perform();
+
+        // Tab
+        actions.keyDown(Keys.TAB).keyUp(Keys.TAB).perform();
+        actions.sendKeys(Keys.TAB).perform();
+
+
+        // Control v
+        actions.keyDown(Keys.CONTROL)
+                .sendKeys("V")
+                .keyUp(Keys.CONTROL)
+                .perform();
+
+
+    }
 }

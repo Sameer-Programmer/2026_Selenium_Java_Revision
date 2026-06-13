f3d239e472c81818711c3708173e4e9c2101ec52
Sameer <sameeridea7@gmail.com>
2026-06-13 23:13:35 +0530

    Add comprehensive explanations for abstract classes, methods, and interfaces; enhance final and super keyword documentation; update wrapper class examples and tests for better clarity and functionality.

--- src/main/java/WrapperClass_Concept/Test2.java
+++ src/main/java/WrapperClass_Concept/Test2.java
@@ -1,4 +1,25 @@
 package WrapperClass_Concept;
 
 public class Test2 {
+    static void main(String[] args) {
+        Integer in1 = 10;
+//System.out.println(in1);
+       // System.out.println(in1.doubleValue());
+
+        String s1 = "100.50";
+//System.out.println(s1);
+
+      //  int x  = Integer.parseInt(s1);
+        //   System.out.println(x);  // invalid compile time
+        double d = Double.parseDouble(s1);
+        System.out.println(d);
+
+
+        String s2 = "xtrue";
+        boolean b1 = Boolean.parseBoolean(s2);
+        System.out.println(b1);
+
+
+
+    }
 }
--- src/main/java/WrapperClass_Concept/Test3.java
+++ src/main/java/WrapperClass_Concept/Test3.java
@@ -1,4 +1,18 @@
 package WrapperClass_Concept;
 
 public class Test3 {
+    static void main(String[] args) {
+
+        int a =10;
+        float f = 1.45f;
+        char ch= 'a';
+        double d = 40.50;
+        long l = 3000;
+        boolean b = true;
+
+        String s1 = String.valueOf(b);
+        System.out.println(s1);
+
+
+    }
 }

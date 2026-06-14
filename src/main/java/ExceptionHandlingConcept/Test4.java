122d492988fef34e081388713593f999cfb586d7
Sameer <sameeridea7@gmail.com>
2026-06-14 16:01:09 +0530

    Add Test4 class implementation and enhance exception handling notes in Page2_Exception; create Xpath_1.html for XPath examples

--- src/main/java/ExceptionHandlingConcept/Test4.java
+++ src/main/java/ExceptionHandlingConcept/Test4.java
@@ -1,4 +1,16 @@
 package ExceptionHandlingConcept;
 
 public class Test4 {
+    static void main() throws InterruptedException {
+        System.out.println("sameer");
+        Thread.sleep(5000);
+        System.out.println("Done");
+    }
 }
+
+
+/*
+Here throws Declares that that Exception might occur
+
+but execption is Handle by whom?
+ */

cb5f8cf2ffa2c8a6d1ebe32522d47d9936703fd4
Sameer <sameeridea7@gmail.com>
2026-06-19 17:56:33 +0530

    Update Maven compiler version to 26 and add detailed file upload notes and test cases for Selenium

--- src/test/java/Sameer_Fun/Test11_FileUploads.java
+++ src/test/java/Sameer_Fun/Test11_FileUploads.java
@@ -1,4 +1,31 @@
 package Sameer_Fun;
 
-public class Test11_FileUploads {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class Test11_FileUploads extends Baseclass_AutomationPractice{
+
+
+@Test(enabled = false)
+    public void singleFileUpload(){
+    WebElement chooseFile1 = driver.findElement(By.id("singleFileInput"));
+    String projectpath = System.getProperty("user.dir");
+    chooseFile1.sendKeys(projectpath+"//Files//Patient.txt");
+}
+
+
+    @Test()
+    public void MultipleFilesUplaod(){
+    WebElement multipleFilesChoose = driver.findElement(By.cssSelector("#multipleFilesInput"));
+        JavascriptExecutor  js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView()",multipleFilesChoose);
+        String projectpath = System.getProperty("user.dir");
+        String file1 = projectpath+"//Files//Patient.txt";
+        String file2 = projectpath+"//Files//Patient.txt";
+        multipleFilesChoose.sendKeys(file1+"\n"+file2);
+    }
+
+
 }

d3efbc1b20a5e039eb4f2fa92da72b9cec17cb4b
Sameer <sameeridea7@gmail.com>
2026-06-20 02:51:26 +0530

    Refactor web table handling tests and add dynamic header search functionality

--- src/test/java/Sameer_Fun/Test15_WebTableScenario1.java
+++ src/test/java/Sameer_Fun/Test15_WebTableScenario1.java
@@ -1,8 +1,12 @@
 package Sameer_Fun;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test15_WebTableScenario1 extends Baseclass_AutomationPractice{
 
@@ -46,4 +50,50 @@ public class Test15_WebTableScenario1 extends Baseclass_AutomationPractice{
         }
 
     }
-}
+
+
+    @Test
+    public void  m2(){
+        int rows=  driver.findElements(By.xpath(" //table[@name='BookTable']//tr")).size();
+        int columns = driver.findElements(By.xpath("//table[@name='BookTable']//th")).size();
+        System.out.println("rows   "+rows);
+        System.out.println("columns  "+columns);
+
+            List<WebElement> elements =driver.findElements(By.xpath("//table[@name='BookTable']//tr//th"));
+                         ArrayList list1 = new ArrayList();
+                    for(WebElement value :elements){
+                        list1.add(value.getText());
+                    }
+
+                    int indexofAuther =list1.indexOf("Author");
+                    int indexUpdatedAuthor = indexofAuther+1;
+        System.out.println(indexUpdatedAuthor);
+
+
+        for(int i = 2; i<=rows; i++){
+            // //table[@name='BookTable']//tr[2]//td[1]
+            String author =
+                    driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td["+indexUpdatedAuthor+"]"))
+                            .getText();
+            if(author.equalsIgnoreCase("Animesh")){
+                String bookName =
+                        driver.findElement(By.xpath("//table[@name='BookTable']//tr["+i+"]//td["+indexUpdatedAuthor+"]"))
+                                .getText();
+                System.out.println(bookName);
+
+                if(bookName.equalsIgnoreCase("Learn JS")){
+                    Assert.assertTrue(true);
+                }
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+} //class
+

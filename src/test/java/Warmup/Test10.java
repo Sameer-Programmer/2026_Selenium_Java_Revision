92bdabb6936225e45e731c50da8699b6fc51a97b
Sameer <sameeridea7@gmail.com>
2026-06-25 23:21:23 +0530

    Refactor TestNG configurations and update test cases
    - Updated workspace.xml to remove outdated test cases and add new ones.
    - Created a new XML structure for TestNG suite in page7_Notes_XmlFile_Structure.
    - Implemented Test10 class to support parameterized browser testing using Selenium.
    - Renamed Test9 to Test9_ReadtheExcelFile and updated its functionality.
    - Created a new TestNg.xml file for the Warmup test suite.
    - Updated test output files to reflect changes in test execution and results.
    - Added new HTML and XML reports for the DemoParametertest.

--- src/test/java/Warmup/Test10.java
+++ src/test/java/Warmup/Test10.java
@@ -1,4 +1,37 @@
 package Warmup;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class Test10 {
+    WebDriver driver;
+    @Parameters("browser")
+    @BeforeClass(groups = {"Master"})
+    public void m1(String browser) {
+
+        switch (browser.toLowerCase()){
+            case "chrome" : driver = new ChromeDriver();break;
+            case "edge" : driver = new EdgeDriver();break;
+            default:
+                System.out.println("Select chrome or edge");
+                return;
+        }
+
+        driver.get("https://tutorialsninja.com/demo/");
+
+    }
+
+    @Test(groups = {"Master"})
+    public void m1(){
+        WebElement element = driver.findElement(By.xpath("//a[text()='Qafox.com']"));
+        Assert.assertTrue(element.isDisplayed());
+        driver.close();
+    }
 }
--- src/test/java/Warmup/Test9_ReadtheExcelFile.java
+++ src/test/java/Warmup/Test9_ReadtheExcelFile.java
@@ -4,16 +4,13 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class Test9 {
+public class Test9_ReadtheExcelFile {
     @Test
     public void m1() throws IOException {
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+ File.separator+"TestData//TestDataFile2.xlsx");
